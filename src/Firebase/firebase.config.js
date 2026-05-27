e1745cda173c9a4007b2c602fdae177cfc648171
Ephraim Elakhe <elakhe.ephraim.9@gmail.com>
2026-05-27 16:04:07 +0100

    update config files

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -1,10 +1,16 @@
+// lib/firebase.js
+
+import { initializeApp } from "firebase/app";
+
 const firebaseConfig = {
-  apiKey: "AIzaSyBCFn_8NN9sipdKO0SkxIxValVVtbm1Fxs",
-  authDomain: "fashion-house-italyl-client.firebaseapp.com",
-  projectId: "fashion-house-italyl-client",
-  storageBucket: "fashion-house-italyl-client.appspot.com",
-  messagingSenderId: "815289028632",
-  appId: "1:815289028632:web:d4032299166f0924dc736d",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
-export default firebaseConfig;
+const app = initializeApp(firebaseConfig);
+
+export default app;
