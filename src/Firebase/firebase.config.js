a456681df6d1409b8f967632b16a62249aee421e
Ephraim Elakhe <elakhe.ephraim.9@gmail.com>
2026-05-27 20:39:47 +0100

    update environment var

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -1,15 +1,10 @@
-// lib/firebase.js
-
-import { initializeApp } from "firebase/app";
-
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyCIrupQXTVIPGrL0s9bEQwQVhrTFowQZpE",
+  authDomain: "norexfashion-ecef4.firebaseapp.com",
+  projectId: "norexfashion-ecef4",
+  storageBucket: "norexfashion-ecef4.firebasestorage.app",
+  messagingSenderId: "823202333421",
+  appId: "1:823202333421:web:18682da315db0d63e682e4"
 };
 
-
-export default app;
+export default firebaseConfig;
